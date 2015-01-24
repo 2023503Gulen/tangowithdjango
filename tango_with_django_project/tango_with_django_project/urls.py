c8a34e89ca9bf08fead0506bd602d415c3126afb
Insay <guleninsay@gmail.com>
2015-01-24 16:23:04 +0000

    4.7. Exercises completed

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'tango_with_django_project.views.home', name='home'),
     url(r'^rango/', include('rango.urls')), # Connecting the main project to rango app.                   
 )
